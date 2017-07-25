da52839cb8c59919d36a9fedbce1e551fa7d4b31
wangying <wangying@ilongyuan.com.cn>
2017-07-25 17:25:32 +0800

    fix some broken package path

--- vendor/github.com/drone/envsubst/template.go
+++ vendor/github.com/drone/envsubst/template.go
@@ -5,7 +5,7 @@ import (
 	"io"
 	"io/ioutil"
 
-	"github.com/smartwang/envsubst/parse"
+	"github.com/drone/envsubst/parse"
 )
 
 // state represents the state of template execution. It is not part of the
