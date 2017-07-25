458e83b11e751ee2f67ba60a1c1db4a861a06c07
wangying <wangying@ilongyuan.com.cn>
2017-07-25 19:19:48 +0800

    bug fix

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ cd ${GOPATH}/src/github.com/smartwang/drone-ui
 git pull
 git checkout develop
 cd -
-go build -ldflags '-extldflags "-static" -X drone/version.VersionDev=build.'${DRONE_BUILD_NUMBER:-$(date +%s)}  -o release/drone-server  github.com/drone/drone/cmd/drone-server
-CGO_ENABLED=0 go build -o release/drone-agent github.com/drone/drone/cmd/drone-agent
+go build -ldflags '-extldflags "-static" -X drone/version.VersionDev=build.'${DRONE_BUILD_NUMBER:-$(date +%s)}  -o release/drone-server  github.com/smartwang/drone/cmd/drone-server
+CGO_ENABLED=0 go build -o release/drone-agent github.com/smartwang/drone/cmd/drone-agent
 
 docker build -f Dockerfile -t registry.cn-hangzhou.aliyuncs.com/ly_ops/drone-server ../drone
 docker build -f Dockerfile.agent -t registry.cn-hangzhou.aliyuncs.com/ly_ops/drone-agent ../drone
